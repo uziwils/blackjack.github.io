28ad684fbb1844bbfcdc7ef9dcc5b2699a8e22b8
Uziah Wilson <uziahwilson@Uziahs-MacBook-Air.local>
2025-03-05 21:52:51 -0700

    updated logic to index.js to feed the proper cards to html file during play

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-/* sets up the necessary values and suits to be dispalyed for the game to run */
+/* sets up the necessary values and suits to be displayed for the game to run
+* TODO: develop on the game logic to hide the dealer's second card */
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a'];
 let deck = [];
 let player1Hand = [];
 let player2Hand = [];
 let currentPlayer = 1;
 let dealerHand = [];
 
+/* creates a deck to be pulled from to deal the game */
 function createDeck() {
     deck = [];
     for (let suit of suits) {
@@ -17,6 +19,14 @@ function createDeck() {
     deck = shuffle(deck);
 }
 
+/* feeds the string for html to pass to html depending on the hand that is dealt
+   which will be implemnted in the function, updateDisplay()
+ */
+function getCardImage(card) {
+    return `img/${card.suit}/${card.value}.png`;
+}
+
+/* shuffles the generated deck from createDeck */
 function shuffle(deck) {
     for (let i = deck.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
@@ -25,17 +35,22 @@ function shuffle(deck) {
     return deck;
 }
 
+/* pop the card from the generated deck to be dealt to player */
 function dealCard(hand) {
     hand.push(deck.pop());
 }
 
+/* function gets the value of cards. accounts for j, q, and k to be 10. on first ace dealt
+    increases the value to 11. after hit however, will decrease by 10 and aces will be
+    counted as 1 instead per black jack rules
+ */
 function getHandValue(hand) {
     let value = 0;
     let aces = 0;
     for (let card of hand) {
-        if (['J', 'Q', 'K'].includes(card.value)) {
+        if (['j', 'q', 'k'].includes(card.value)) {
             value += 10;
-        } else if (card.value === 'A') {
+        } else if (card.value === 'a') {
             aces++;
             value += 11;
         } else {
@@ -49,6 +64,9 @@ function getHandValue(hand) {
     return value;
 }
 
+/* starts the game by creating the deck, creating a fresh list to hold the hands for
+    player 1, 2, and dealer, then deal the cards
+ */
 function startGame() {
     createDeck();
     player1Hand = [];
@@ -63,15 +81,20 @@ function startGame() {
     updateDisplay();
 }
 
+/* function to hit */
 function hit() {
+    // checks to see if the first player is in play. deals to apropiate player
     if (currentPlayer === 1) {
         dealCard(player1Hand);
         updateDisplay();
+        // continues until the player busts
         if (getHandValue(player1Hand) > 21) {
             document.getElementById('message').innerText = 'You Busted! Dealer Wins!';
             currentPlayer = 2;
         }
-    } else{
+    }
+    // plays player2's hand
+    else{
         dealCard(player2Hand);
         updateDisplay();
         if (getHandValue(player2Hand) > 21) {
@@ -81,15 +104,19 @@ function hit() {
     }
 }
 
+// function that ends the respective player's turn
 function stand() {
     if (currentPlayer === 1) {
         currentPlayer = 2;
         document.getElementById('message').innerText = 'Player 2\'s Turn!';
-    } else {
+    }
+    // if player 2 is in play and stands, jumps to player2
+    else {
         dealerTurn();
     }
 }
 
+// function that plays dealer's hand. stands on 17
 function dealerTurn() {
     while (getHandValue(dealerHand) < 17) {
         dealCard(dealerHand);
@@ -98,18 +125,29 @@ function dealerTurn() {
     determineWinner();
 }
 
+/* determines the winner */
 function determineWinner() {
+    // functions gets the playervalue of player 1, 2, and dealer
     let player1Value = getHandValue(player1Hand);
     let player2Value = getHandValue(player2Hand);
     let dealerValue = getHandValue(dealerHand);
+
+    // reests the player counter to player 1 for next turn
     currentPlayer = 1;
+
+    // creates an empty string to be passed to the html
     let message = '';
 
+    // checks if dealer busted. if so, had player1, and 2 have not busted, both automatically
+    // win their hand
     if (dealerValue > 21) {
         message = 'Dealer Busted! ';
         if (player1Value <= 21) message += 'Player 1 Wins! ';
         if (player2Value <= 21) message += 'Player 2 Wins!';
-    } else {
+    }
+    // if dealer has not busted, checks the values to see if the player's win based on beating
+    // the dealer's hand, tied, or if the dealer beats their hand
+    else {
         if (player1Value > dealerValue && player1Value <= 21) {
             message += 'Player 1 Wins! ';
         } else if (player1Value === dealerValue) {
@@ -127,18 +165,52 @@ function determineWinner() {
         }
     }
 
+    // updates the message on html file
     document.getElementById('message').innerText = message;
 }
 
+/* function updates the html display. checks the player's current hand and deals
+   retrieves the proper card based on the value and suit the player has.
+ */
 function updateDisplay() {
-    document.getElementById('player1-hand').innerText = JSON.stringify(player1Hand);
-    document.getElementById('player2-hand').innerText = JSON.stringify(player2Hand);
-    document.getElementById('dealer-hand').innerText = JSON.stringify(dealerHand);
+    const player1Container = document.getElementById('player1-hand');
+    const player2Container = document.getElementById('player2-hand');
+    const dealerContainer = document.getElementById('dealer-hand');
+
+    player1Container.innerHTML = '';
+    player2Container.innerHTML = '';
+    dealerContainer.innerHTML = '';
+
+    // following pulls together the string for to feed the images for the cards to
+    // html end
+    player1Hand.forEach(card => {
+        const img = document.createElement('img');
+        img.src = getCardImage(card);
+        img.classList.add('card');
+        player1Container.appendChild(img);
+    });
+
+    player2Hand.forEach(card => {
+        const img = document.createElement('img');
+        img.src = getCardImage(card);
+        img.classList.add('card');
+        player2Container.appendChild(img);
+    });
+
+    dealerHand.forEach(card => {
+        const img = document.createElement('img');
+        img.src = getCardImage(card);
+        img.classList.add('card');
+        dealerContainer.appendChild(img);
+    });
+
+    // updates the players and dealer score on html end
     document.getElementById('player1-score').innerText = getHandValue(player1Hand);
     document.getElementById('player2-score').innerText = getHandValue(player2Hand);
     document.getElementById('dealer-score').innerText = getHandValue(dealerHand);
 }
 
+// listeners to check for clicks on the buttons to start game, hit, and stand
 document.getElementById('start').addEventListener('click', startGame);
 document.getElementById('hit').addEventListener('click', hit);
 document.getElementById('stand').addEventListener('click', stand);
